119b59feb02a7b28d575709f78e77a2d5bdaf2c3
Nicholas Allee <42816286+jiggensnaggen@users.noreply.github.com>
2020-02-19 09:06:55 -0600

    Add project files.

---
+++ numberChangerWithDelegates/numberChanger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace numberChangerWithDelegates
+{
+    class numberChanger
+    {
+        static int num = 10;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            theNumberChanger sol = new theNumberChanger(add);
+            Console.WriteLine(sol(2));
+            theNumberChanger subtracting = new theNumberChanger(sub);
+            Console.WriteLine(subtracting(8));
+            theNumberChanger multiplying = new theNumberChanger(mult);
+            Console.WriteLine(multiplying(2));
+        }
+
+        public static int add(int n)
+        {
+            num = n +num;
+            return num;
+        }
+
+        public static int sub(int n)
+        {
+            num = num - n;
+            return num;
+        }
+
+        public static int mult(int n)
+        {
+            num = n * num;
+            return num;
+        }
+
+        public static int div(int n)
+        {
+            num = n / num;
+            return num;
+        }
+
+        public delegate int theNumberChanger(int number);
+
+    }
+}
